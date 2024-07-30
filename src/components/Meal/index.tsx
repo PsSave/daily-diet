758927a1ba53adacfbbaf17eb82150f25cdbf2b2
Pedro Samuel <pedro.samuel@onesolutions.com.br>
2024-07-30 14:10:45 -0300

    chore: Add Meal component for displaying individual meals

---
+++ src/components/Meal/index.tsx
@@ -0,0 +1,20 @@
+import { TouchableOpacityProps } from "react-native";
+import * as S from "./styles";
+
+type MealProps = {
+  hour: string;
+  title: string;
+  status?: S.status;
+};
+
+export default function Meal({ hour, title, status = true }: MealProps) {
+  console.log(hour, title, status);
+  return (
+    <S.Container activeOpacity={0.7}>
+      <S.Hour>{hour}</S.Hour>
+      <S.Divider />
+      <S.Title>{title}</S.Title>
+      <S.Status status={status} />
+    </S.Container>
+  );
+}
---
+++ src/components/Meal/styles.ts
@@ -0,0 +1,58 @@
+import { TouchableOpacity } from "react-native";
+import styled, { css } from "styled-components/native";
+
+export type status = true | false;
+
+type Props = {
+  status: status;
+};
+
+export const Container = styled(TouchableOpacity)`
+  width: 100%;
+  height: 49px;
+
+  flex-direction: row;
+  align-items: center;
+
+  border: 1px solid ${({ theme }) => theme.COLORS.GRAY_5};
+  border-radius: 6px;
+
+  margin-bottom: 8px;
+`;
+
+export const Hour = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZES.SM}px;
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.GRAY_1};
+    margin-left: 16px;
+  `}
+`;
+
+export const Divider = styled.View`
+  width: 1px;
+  height: 14px;
+  margin-left: 12px;
+  margin-right: 12px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_5};
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    flex: 1;
+    font-size: ${theme.FONT_SIZES.MD}px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    color: ${theme.COLORS.GRAY_2};
+  `}
+`;
+
+export const Status = styled.View<Props>`
+  ${({ theme, status }) => css`
+    width: 14px;
+    height: 14px;
+    border-radius: 25px;
+    background-color: ${status ? theme.COLORS.GREEN_MID : theme.COLORS.RED_MID};
+    margin-left: auto;
+    margin-right: 16px;
+  `}
+`;
