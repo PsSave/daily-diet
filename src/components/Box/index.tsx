b2c6465a787bd5c3d59a88570b847626c676a273
Pedro Samuel <pedro.samuel@onesolutions.com.br>
2024-07-31 19:12:33 -0300

    chore: Refactor Box component to support additional props

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,20 +1,36 @@
+import React from "react";
 import * as S from "./styles";
+import { ViewProps } from "react-native";
 
-type BoxProps = {
-  isShowHidden?: boolean;
+type BoxProps = ViewProps & {
   type: S.Variants;
+  percent: string;
+  description: string;
+  iconPosition?: S.IconPosition;
+  isShowHidden?: boolean;
 };
 
-export default function Box({ type, isShowHidden = false }: BoxProps) {
+export default function Box({
+  type,
+  percent,
+  description,
+  iconPosition = "right",
+  isShowHidden = false,
+  ...rest
+}: BoxProps) {
   return (
-    <S.Container type={type}>
+    <S.Container type={type} {...rest}>
       {isShowHidden && (
-        <S.ButtonIcon activeOpacity={0.7}>
-          <S.Icon type={type} />
+        <S.ButtonIcon position={iconPosition} activeOpacity={0.7}>
+          {iconPosition === "left" ? (
+            <S.ArrowLeftIcon type={type} />
+          ) : (
+            <S.ArrowUpRightIcon type={type} />
+          )}
         </S.ButtonIcon>
       )}
-      <S.Percent>90,86%</S.Percent>
-      <S.Description>das refeições dentro da dieta</S.Description>
+      <S.Percent>{percent}</S.Percent>
+      <S.Description>{description}</S.Description>
     </S.Container>
   );
 }
--- src/components/Box/styles.ts
+++ src/components/Box/styles.ts
@@ -1,20 +1,27 @@
-import { ArrowUpRight } from "lucide-react-native";
+import { ArrowLeft, ArrowUpRight } from "lucide-react-native";
 import { TouchableOpacity } from "react-native";
 import styled, { css } from "styled-components/native";
 
-export type Variants = "primary" | "secondary";
+export type Variants = "primary" | "secondary" | "basic";
+export type IconPosition = "left" | "right";
 
 type Props = {
-  type: Variants;
+  type?: Variants;
+  position?: IconPosition;
 };
 
 export const Container = styled.View<Props>`
-  width: 100%;
   height: 107px;
+  padding: 16px;
+
   background-color: ${({ theme, type }) =>
-    type === "primary" ? theme.COLORS.GREEN_LIGHT : theme.COLORS.RED_LIGHT};
+    type === "primary"
+      ? theme.COLORS.GREEN_LIGHT
+      : type === "secondary"
+      ? theme.COLORS.RED_LIGHT
+      : theme.COLORS.GRAY_6};
   border-radius: 8px;
-  margin-top: 33px;
+
   justify-content: center;
   align-items: center;
   gap: 2px;
@@ -38,16 +45,28 @@ export const Description = styled.Text`
   `}
 `;
 
-export const ButtonIcon = styled(TouchableOpacity)`
-  position: absolute;
-  top: 10px;
-  right: 10px;
+export const ButtonIcon = styled(TouchableOpacity)<Props>`
+  ${({ position }) => css`
+    position: absolute;
+    top: 10px;
+    right: ${position === "right" ? 10 : ""}px;
+    left: ${position === "left" ? 10 : ""}px;
 
-  justify-content: center;
-  align-items: center;
+    justify-content: center;
+    align-items: center;
+  `}
 `;
 
-export const Icon = styled(ArrowUpRight).attrs<Props>(({ theme, type }) => ({
-  size: theme.FONT_SIZES.XL,
-  color: type === "primary" ? theme.COLORS.GREEN_DARK : theme.COLORS.RED_DARK,
-}))``;
+export const ArrowUpRightIcon = styled(ArrowUpRight).attrs<Props>(
+  ({ theme, type }) => ({
+    size: theme.FONT_SIZES.XL,
+    color: type === "primary" ? theme.COLORS.GREEN_DARK : theme.COLORS.RED_DARK,
+  })
+)``;
+
+export const ArrowLeftIcon = styled(ArrowLeft).attrs<Props>(
+  ({ theme, type }) => ({
+    size: theme.FONT_SIZES.XL,
+    color: type === "primary" ? theme.COLORS.GREEN_DARK : theme.COLORS.RED_DARK,
+  })
+)``;
