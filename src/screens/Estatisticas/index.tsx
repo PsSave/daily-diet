71883b1e474ec5bb37a096dda7086afbbd6f0926
Pedro Samuel <pedro.samuel@onesolutions.com.br>
2024-07-31 19:13:35 -0300

    feat: Add Estatisticas screen and styles

---
+++ src/screens/Estatisticas/index.tsx
@@ -0,0 +1,10 @@
+import LayoutStatistic from "@components/LayoutStatistic";
+import * as S from "./styles";
+
+export default function Estatisticas() {
+  return (
+    <S.Container>
+      <LayoutStatistic />
+    </S.Container>
+  );
+}
